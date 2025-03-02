cac117667a0636508736d285010ec8f1fa37b6b3
chaitanya kumar reddy <bhimireddy.chaitanyakumarreddy@gmail.com>
2025-03-02 15:46:57 +0530

    this is modified files java programs

---
+++ java/methods.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class chaitanya{
+    chaitanya(){
+     int a =10;
+ int b=20;
+  System.out.println(a+b);
+   }
+  public static void main(String args[]){
+  chaitanya obj = new chaitanya();
+    
+  
+}
+}
