ac1fabd7c697007492cb685ba1dac913aa83bb28
chaitanya kumar reddy <bhimireddy.chaitanyakumarreddy@gmail.com>
2025-03-02 11:53:51 +0530

    this html file and in this addition program is there in that file

---
+++ html/addition.java
@@ -0,0 +1,11 @@
+package html;
+
+public class addition {
+    public static void main(String args[]){
+        int a =10;
+        int b =20;
+        System.out.println("this is addition program = "+ (a+b));
+
+    }
+    
+}
